ab01bbf1609131575f9ea8a803f86334c82eb246
Daseul1 <chlektmf4411@gmail.com>
2024-06-12 09:23:37 +0900

    Initial commit

---
+++ src/variable/Var3.java
@@ -0,0 +1,12 @@
+package variable;
+
+public class Var3 {
+    public static void main(String[] args) { // psvm 으로 작성할 수 있음
+        int a;
+        a = 10;
+        System.out.println(a); //sout 으로 작성할 수 있음
+
+        a=50;
+        System.out.println(a);
+    }
+}
