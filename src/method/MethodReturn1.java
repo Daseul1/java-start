d75cefca80efb97a4bc378c9e53ecf22bcb727aa
Daseul1 <chlektmf4411@gmail.com>
2024-06-21 16:20:08 +0900

    메서드 호출과 값 전달1 진행중

---
+++ src/method/MethodReturn1.java
@@ -0,0 +1,22 @@
+package method;
+
+public class MethodReturn1 {
+    public static void main(String[] args) {
+        boolean result = odd(2);
+        System.out.println(result);
+    }
+
+    public static boolean odd(int i) {
+        if (i % 2 == 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+/*
+ if 조건이 만족할 때는 true가 반환되지만,
+ 조건을 만족하지 않으면 return 문이 실행되지 않아서
+ return 문을 누락했다는 컴파일 오류(java: missing return statement) 발생
+*/
